73dc7feb1780edf5b51af0a4744c58a206433233
IsaacSherman <sherman1337@gmail.com>
2016-04-28 16:34:24 -0400

    Preprocessing mostly done.

--- auxilliary.py
+++ auxilliary.py
@@ -1,19 +1,30 @@
-import numpy as np
-import scipy as sp
-from numpy import core
-from numpy.core import multiarray
+import pickle
 
 
-def findBetween(longText, begin, end):
+def loadTitleFile(filename='simplewiki-20160305-all-titles'):
+    retlist = []
+    retdict = {}
+    count = 0
+    with open(filename, 'r', encoding='utf8') as fin:
+        for line in fin.readlines():
+            modded = line.strip()
+            retdict[modded] = count
+            retlist.append(modded)
+            count += 1
+    return retdict, retlist
+
+
+def findBetween(longText, begin, end, ignoreTrailingSymbols=False):
     ret = []
     start = 0
-    while(1):
+    while (1):
         start = longText.find(begin, start) + len(begin)
-        if start == len(begin)-1:
+        if start == len(begin) - 1:
             break
         last = longText.find(end, start)
         if last == -1:
-            ret.append(longText[start:])#found the open token, not the close.  May want to adjust behavior
+            if not ignoreTrailingSymbols:
+                ret.append(longText[start:])  # found the open token, not the close.
             break
         ret.append(longText[start:last])
     return ret
@@ -25,8 +36,10 @@ def parseFile(contents, tokens, ret):
         ret[tokens['open']] = findBetween(contents, tokens['open'], tokens['close'])
     return ret
 
+
 def turnIntoTokenStruct(openToken, closeToken):
     return {'open': openToken, 'close': closeToken}
+
 
 def testAux():
     longText = "This is a long sentence<!> with <b>random</b> tags in <i>it which don't make much" \
@@ -45,6 +58,130 @@ def testAux():
         print("Token " + token['open'] + ": ", tokenDict[token['open']])
 
 
-testAux()
+def preprocesslevel1(filename="simplewiki-20160305-pages-meta-current.xml"):
+    openTokens = ["<title>", '<text xml:space']
+    closeTokens = ["</title>", '</text>']
+    tokens = []
+    for i in range(0, len(openTokens)):
+        tokens.append(turnIntoTokenStruct(openTokens[i], closeTokens[i]))
 
-#https://gist.github.com/diogojc/1338222
+    with open("superStripped.txt", 'w', encoding='utf8') as fout:
+        with open(filename, 'r', encoding='utf8') as file:
+            on = False
+            for line in file.readlines():
+                for token in tokens:
+                    if token['open'] in line:
+                        on = True
+                        fout.write(line)
+                    elif token['close'] in line:
+                        on = False
+                        fout.write(line)
+                if on:
+                    start = line.find('[[')
+                    end = line.find(']]')
+                    if (start > -1):
+                        fout.write(line[start:])
+
+
+def preprocessLevel2(file='superStripped.txt'):
+    tokens = []
+    delim = "!!!|!!!"
+    tokenDict = {}
+    tokens.append(turnIntoTokenStruct('[[', ']]'))
+    with open(file, 'r', encoding='utf8') as fin:
+        with open('minimal.txt', 'w', encoding='utf8') as fout:
+            for line in fin.readlines():
+                if '<title>' in line:
+                    start = line.find('<title>') + 7
+                    end = line.find('</title>')
+                    fout.write(line[start:end] + delim)
+                    tokenDict = {}
+
+                for token in tokens:
+                    tokenDict = parseFile(line, token, tokenDict)
+                for item in tokenDict[tokens[0]['open']]:
+                    if "File:" in item:
+                        continue
+                    item = item.replace(' ', '_')
+                    item = item.replace("\n", "")
+                    end = item.find('|')
+                    if end == -1:
+                        end = len(item.strip())
+                    fout.write(item[0:end])
+                    fout.write(', ')
+                if '</text>' in line:
+                    fout.write('\n')
+                    tokenDict = {}
+
+
+def preprocessLevel3(filename='minimal.txt'):
+    ret = {}
+    delim = "!!!|!!!"
+    with open(filename, 'r', encoding='utf8') as fin:
+        for line in fin.readlines():
+            line = line.strip()
+            while line.find(delim) != line.rfind(delim):
+                print("Stripping delim from line:", line)
+                start = line.find(delim)
+                line = line[0:start + len(delim)] + line[start + len(delim):].replace(delim, '')
+            if delim not in line:
+                print("delim Not found", line)
+                continue
+            title, linkstr = line.split(delim)
+            links = linkstr.split(', ')
+            if title in ret.keys():
+                print("duplicate title found:", title)
+            else:
+                ret[title] = []
+            for link in links:
+                if (link in ret[title]):
+                    pass
+                else:
+                    ret[title].append(link)
+    return ret
+
+
+# preprocesslevel1()
+# preprocessLevel2()
+try:
+    linkDict = pickle.load(open('linkDict.pkl', 'rb'))
+except IOError:
+    linkDict = preprocessLevel3()
+    pickle.dump(linkDict, open('linkDict.pkl', 'wb'))
+
+
+
+def intifyDicts():
+    global intDict
+    titleDict, titleArray = loadTitleFile()
+    intDict = {}
+
+    def intifyList(strlist, dict):
+        ret = []
+        for string in strlist:
+            if string in ret:
+                continue
+            else:
+                ret.append(dict[string])
+        ret.sort()
+        return ret
+
+    for key in linkDict.keys():
+        intDict[titleDict[key]] = intifyList(titleDict[key], titleDict)
+
+testAux()
+try:
+    pickle.load(open('intDict.pkl','rb'))
+except IOError:
+    intDict = intifyDicts()
+    pickle.dump(intDict, open('intDict.pkl', 'wb'))
+
+with open('titleLen.txt', 'w') as lenFile:
+    lenFile.write(len(intDict.keys()))
+
+
+
+
+
+
+# https://gist.github.com/diogojc/1338222
